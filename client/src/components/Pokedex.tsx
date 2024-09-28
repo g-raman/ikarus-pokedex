c4c859c2d94e3e937712473e82f50b1e32695dc7
Raman Gupta <gr.gupta.raman@gmail.com>
2024-09-28 18:31:46 -0400

    feat: fetch data from api and remove dummy data

--- client/src/components/Pokedex.tsx
+++ client/src/components/Pokedex.tsx
@@ -1,189 +1,7 @@
 import { Table, TableColumnsType } from "antd";
 import { Pokemon } from "../utils/types";
 import { PokemonTypeBadge } from "./PokemonTypeBadge";
-
-const data = [
-  {
-    id: 1,
-    name: {
-      english: "Bulbasaur",
-      japanese: "フシギダネ",
-      chinese: "妙蛙种子",
-      french: "Bulbizarre",
-    },
-    type: ["Bug", "Poison"],
-    base: {
-      HP: 45,
-      Attack: 49,
-      Defense: 49,
-      SpAttack: 65,
-      SpDefense: 65,
-      Speed: 45,
-    },
-  },
-  {
-    id: 2,
-    name: {
-      english: "Ivysaur",
-      japanese: "フシギソウ",
-      chinese: "妙蛙草",
-      french: "Herbizarre",
-    },
-    type: ["Grass", "Poison"],
-    base: {
-      HP: 60,
-      Attack: 62,
-      Defense: 63,
-      SpAttack: 80,
-      SpDefense: 80,
-      Speed: 60,
-    },
-  },
-  {
-    id: 3,
-    name: {
-      english: "Venusaur",
-      japanese: "フシギバナ",
-      chinese: "妙蛙花",
-      french: "Florizarre",
-    },
-    type: ["Grass", "Poison"],
-    base: {
-      HP: 80,
-      Attack: 82,
-      Defense: 83,
-      SpAttack: 100,
-      SpDefense: 100,
-      Speed: 80,
-    },
-  },
-  {
-    id: 4,
-    name: {
-      english: "Charmander",
-      japanese: "ヒトカゲ",
-      chinese: "小火龙",
-      french: "Salamèche",
-    },
-    type: ["Fire"],
-    base: {
-      HP: 39,
-      Attack: 52,
-      Defense: 43,
-      SpAttack: 60,
-      SpDefense: 50,
-      Speed: 65,
-    },
-  },
-  {
-    id: 5,
-    name: {
-      english: "Charmeleon",
-      japanese: "リザード",
-      chinese: "火恐龙",
-      french: "Reptincel",
-    },
-    type: ["Fire"],
-    base: {
-      HP: 58,
-      Attack: 64,
-      Defense: 58,
-      SpAttack: 80,
-      SpDefense: 65,
-      Speed: 80,
-    },
-  },
-  {
-    id: 6,
-    name: {
-      english: "Charizard",
-      japanese: "リザードン",
-      chinese: "喷火龙",
-      french: "Dracaufeu",
-    },
-    type: ["Fire", "Flying"],
-    base: {
-      HP: 78,
-      Attack: 84,
-      Defense: 78,
-      SpAttack: 109,
-      SpDefense: 85,
-      Speed: 100,
-    },
-  },
-  {
-    id: 7,
-    name: {
-      english: "Squirtle",
-      japanese: "ゼニガメ",
-      chinese: "杰尼龟",
-      french: "Carapuce",
-    },
-    type: ["Water"],
-    base: {
-      HP: 44,
-      Attack: 48,
-      Defense: 65,
-      SpAttack: 50,
-      SpDefense: 64,
-      Speed: 43,
-    },
-  },
-  {
-    id: 8,
-    name: {
-      english: "Wartortle",
-      japanese: "カメール",
-      chinese: "卡咪龟",
-      french: "Carabaffe",
-    },
-    type: ["Water"],
-    base: {
-      HP: 59,
-      Attack: 63,
-      Defense: 80,
-      SpAttack: 65,
-      SpDefense: 80,
-      Speed: 58,
-    },
-  },
-  {
-    id: 9,
-    name: {
-      english: "Blastoise",
-      japanese: "カメックス",
-      chinese: "水箭龟",
-      french: "Tortank",
-    },
-    type: ["Water"],
-    base: {
-      HP: 79,
-      Attack: 83,
-      Defense: 100,
-      SpAttack: 85,
-      SpDefense: 105,
-      Speed: 78,
-    },
-  },
-  {
-    id: 10,
-    name: {
-      english: "Caterpie",
-      japanese: "キャタピー",
-      chinese: "绿毛虫",
-      french: "Chenipan",
-    },
-    type: ["Bug"],
-    base: {
-      HP: 45,
-      Attack: 30,
-      Defense: 35,
-      SpAttack: 20,
-      SpDefense: 20,
-      Speed: 45,
-    },
-  },
-];
+import { gql, useQuery } from "@apollo/client";
 
 const columns: TableColumnsType<Pokemon> = [
   {
@@ -239,6 +57,30 @@ const columns: TableColumnsType<Pokemon> = [
   },
 ];
 
+const QUERY_ALL_POKEMON = gql`
+  query {
+    pokemons(query: "") {
+      id
+      name {
+        english
+      }
+      type
+      base {
+        HP
+        Attack
+        Defense
+        SpAttack
+        SpDefense
+      }
+    }
+  }
+`;
+
 export const Pokedex = () => {
-  return <Table<Pokemon> dataSource={data} columns={columns} />;
+  const { data } = useQuery(QUERY_ALL_POKEMON);
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return <Table<Pokemon> dataSource={data.pokemons} columns={columns} />;
 };
