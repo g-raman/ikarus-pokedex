be09fec1c349f9ee42b022844abb0bc1687a876e
Raman Gupta <gr.gupta.raman@gmail.com>
2024-09-28 15:52:30 -0400

    feat: add search

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,8 @@ import {
 } from "graphql";
 import Pokemon from "./helpers/types";
 
+import { IFuseOptions } from "fuse.js";
+const Fuse = require("fuse.js");
 const express = require("express");
 const { createHandler } = require("graphql-http/lib/use/express");
 const { ruruHTML } = require("ruru/server");
@@ -14,17 +16,32 @@ const cors = require("cors");
 const { pokemonType } = require("./schema/schema.ts");
 const pokedex: [Pokemon] = require("./data/pokedex.json");
 
+const searchOptions: IFuseOptions<Pokemon> = {
+  isCaseSensitive: false,
+  shouldSort: true,
+  findAllMatches: true,
+  ignoreLocation: true,
+  keys: ["name.english"],
+};
+
+const fuse = new Fuse(pokedex, searchOptions);
+
 const queryType = new GraphQLObjectType({
   name: "Query",
   fields: {
     pokemons: {
       type: new GraphQLList(pokemonType),
       args: {
+        query: { type: GraphQLString },
         take: { type: GraphQLInt },
         skip: { type: GraphQLInt },
       },
-      resolve: (_, { take = 10, skip = 0 }) =>
-        pokedex.slice(0 + skip, take + skip),
+      resolve: (_, { query = "", take = 10, skip = 0 }) => {
+        return fuse
+          .search(query)
+          .map((result) => result.item)
+          .slice(0 + skip, take + skip);
+      },
     },
     pokemon: {
       type: pokemonType,
