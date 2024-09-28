592511b977fc5b61090d009e9990b3a11dbcbd6a
Raman Gupta <gr.gupta.raman@gmail.com>
2024-09-28 17:07:12 -0400

    feat: add Pokemon type

---
+++ client/src/utils/types.ts
@@ -0,0 +1,41 @@
+enum PokemonType {
+  Normal,
+  Fire,
+  Water,
+  Electric,
+  Grass,
+  Ice,
+  Fighting,
+  Poison,
+  Ground,
+  Flying,
+  Psychic,
+  Bug,
+  Rock,
+  Ghost,
+  Dragon,
+  Dark,
+  Steel,
+  Fairy,
+}
+
+interface Pokemon {
+  id: number;
+  name: {
+    english: string;
+    japanese: string;
+    chinese: string;
+    french: string;
+  };
+  type: PokemonType[];
+  base: {
+    HP: number;
+    Attack: number;
+    Defense: number;
+    SpAttack: number;
+    SpDefense: number;
+    Speed: number;
+  };
+}
+
+export default Pokemon;
