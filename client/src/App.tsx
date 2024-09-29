b21653dc663b0359c822b31027d69e4c63ffeb22
Raman Gupta <gr.gupta.raman@gmail.com>
2024-09-29 10:56:44 -0400

    feat: use static graphql uri

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PokedexPage } from "./pages/PokedexPage";
 import { PokemonPage } from "./pages/PokemonPage";
 
-const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_ENDPOINT;
 function App() {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: GRAPHQL_ENDPOINT,
+    uri: "http://localhost:3000/graphql",
   });
 
   const router = createBrowserRouter([
